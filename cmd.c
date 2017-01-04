a78cbb8cd63c5dc5cdcab8e00fe725cb07ada797
robinmarechal <robin.marechal-2@etu.univ-tours.fr>
2017-01-04 10:56:44 +0100

    init

---
+++ cmd.c
@@ -0,0 +1,47 @@
+#include "cmd.h"
+//Your includes come here
+
+//Prints the contents of members_args to the console
+void print_members_args(cmd *c){
+    //your implementation comes here
+}
+
+//Frees the memory allocated to store member arguments
+void free_members_args(cmd *c){
+    //your implementation comes here
+}
+
+//Prints the contents of members to the console
+void print_members(cmd *c){
+    //your implementation comes here
+}
+
+//Frees the memory allocated to store member information
+void free_members(cmd *c){
+    //your implementation comes here
+}
+
+//Prints the redirection information for member i
+void print_redirection(cmd c, int i){
+    //your implementation comes here
+}
+
+//Frees the memory allocated to store redirection info
+void free_redirection(cmd *c){
+    //your implementation comes here
+}
+
+//Remplit les champs cmd_args et nb_args_membres
+void parse_members_args(cmd *c){
+    //your implementation comes here
+}
+
+//Remplit les champs initial_cmd, membres_cmd et nb_membres
+void parse_members(char *s,cmd *c){
+    //your implementation comes here
+}
+
+//Remplit les champs redir et type_redir
+void parse_redirection(unsigned int i, cmd *c){
+    //your implementation comes here
+}
